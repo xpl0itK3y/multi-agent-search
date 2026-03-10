99a5e27aa1d6d61f96340980433a854672e536ff
Denis <latundenis55@gmail.com>
2026-03-10 12:45:37 +0500

    Add SQLAlchemy task store implementation

--- src/repositories/__init__.py
+++ src/repositories/__init__.py
@@ -4,9 +4,11 @@ from .mappers import (
     search_task_orm_to_schema,
 )
 from .protocols import TaskStore
+from .sqlalchemy_task_store import SQLAlchemyTaskStore
 
 __all__ = [
     "TaskStore",
+    "SQLAlchemyTaskStore",
     "research_orm_to_record",
     "search_task_orm_to_schema",
     "search_result_dicts_to_orm",
