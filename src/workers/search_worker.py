1b8777f8829cfa691c81943b8bf2cac31712a584
Denis <latundenis55@gmail.com>
2026-03-21 14:12:10 +0500

    Improve worker heartbeat and research progress UI

--- src/workers/search_worker.py
+++ src/workers/search_worker.py
@@ -6,8 +6,9 @@ logger = logging.getLogger(__name__)
 
 
 class SearchWorker:
-    def __init__(self, research_service: ResearchService):
+    def __init__(self, research_service: ResearchService, worker_name: str = "job-worker"):
         self.research_service = research_service
+        self.worker_name = worker_name
 
     def run_once(self) -> int:
         processed = 0
@@ -16,8 +17,18 @@ class SearchWorker:
             job = self.research_service.task_store.claim_next_search_task_job()
             if job is None:
                 break
+            self.research_service.touch_worker_heartbeat(
+                self.worker_name,
+                processed,
+                "busy",
+            )
             logger.info("search_job_claimed job_id=%s task_id=%s depth=%s", job.id, job.task_id, job.depth.value)
             self.research_service.process_search_task_job(job.id)
             processed += 1
+            self.research_service.touch_worker_heartbeat(
+                self.worker_name,
+                processed,
+                "busy",
+            )
 
         return processed
